90e846288790ded68d3a3220513be743976dc935
blckphz <131730464+blckphz@users.noreply.github.com>
2025-09-16 13:55:19 +0200

    added stop zone
    HALT STOP

---
+++ MAMATON/Assets/playerStop.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class playerStop : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the object entering the trigger has the "Player" tag
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player entered the trigger!");
+            // You can stop the player or do other actions here
+            // Example: stop movement script
+            PlayerMove playerMove = other.GetComponent<PlayerMove>();
+            if (playerMove != null)
+            {
+                playerMove.enabled = false; // disables the PlayerMove script
+            }
+        }
+    }
+}
