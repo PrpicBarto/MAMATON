1c3013e82855f584cb767c72e6fd88a3fbc6237e
PrpicBarto <barto.prpic@gmail.com>
2025-09-16 14:47:44 +0200

    Added CameraSequence

---
+++ MAMATON/Assets/_Scripts/Barto/RunCamera.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class RunCamera : MonoBehaviour
+{
+    [SerializeField] CinemachineCamera[] cameras;
+    [SerializeField] GameObject player;
+    [SerializeField] int index;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            SwitchCameras();
+        }
+    }
+
+    void SwitchCameras()
+    {
+        StartCoroutine(CameraSwitch());
+    }
+
+    IEnumerator CameraSwitch()
+    {
+        yield return new WaitForSeconds(0.1f);
+        cameras[0].Priority = index;
+        index++;
+        yield return new WaitForSeconds(1f);
+        cameras[1].Priority = index;
+        index++;
+        yield return new WaitForSeconds(1f);
+        cameras[2].Priority = index;
+        index++;
+        yield return new WaitForSeconds(1f);
+        cameras[0].Priority = index;
+        yield return null;
+    }
+}
